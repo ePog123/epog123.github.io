4f1c881291cf473bb5dd50e17e4db4be9e993e2b
Arnav Gyanani <85708106+ePogYT@users.noreply.github.com>
2022-02-05 14:16:48 -0800

    del all

--- libretro/utils.js
+++
@@ -1,6 +0,0 @@
-module.exports.getExtension = function(filename) {
-  var parts = filename.split('.')
-  extension = parts[parts.length - 1]
-  extension = extension.toLowerCase()
-  return extension
-}
